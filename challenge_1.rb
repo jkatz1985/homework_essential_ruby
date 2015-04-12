b5701033b503334b025aa598f9cf06ebdf61210d
Jacob Katz <jacob-katz@kellogg.northwestern.edu>
2015-04-11 22:48:22 -0500

    part1coimplete3

--- challenge_1.rb
+++ challenge_1.rb
@@ -15,8 +15,8 @@
 
 def pmt(rate, nper, pv)
   # =========================================================
-  # Your code to implement the method goes here.
-  # You shouldn't have to write or change code anywhere else.
+  payment = pv*(rate/(1-(1+rate)**(-nper)))
+  return payment
   # =========================================================
 end
 
@@ -24,7 +24,7 @@ end
 #   and once you have successfully defined it, re-comment them (before
 #   starting challenge_2.rb).
 
-# the_payment = pmt(0.0404/12, 60, 30000)
-# puts "Your monthly payment will be $#{the_payment.round(2)}."
+the_payment = pmt(0.0404/12, 60, 30000)
+puts "Your monthly payment will be $#{the_payment.round(2)}."
 
 # You should get $553.04.
